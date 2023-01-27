2193f76a9675f39fd5939ba2a288c9959d9d1879
SoaB1 <kumachan1230@gmail.com>
2023-01-27 18:09:57 +0900

    fix: helpの表示を変更

--- cmd/root.go
+++ cmd/root.go
@@ -17,13 +17,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go2ssh",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Use go2ssh to connect to your servers",
+	Long: `
+	Use go2ssh to connect to your servers`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
